a6a49f628a440f4b92693c41bb940c44e2c7ba69
Dimitris Greasidis <dgreasidis+github@gmail.com>
2022-06-30 20:59:15 +0300

    feat: add error message on no internet connection

--- src/api/midleware.ts
+++ src/api/midleware.ts
@@ -4,6 +4,7 @@ import { Platform } from 'react-native';
 import { getVersion } from 'react-native-device-info';
 import NetInfo from '@react-native-community/netinfo';
 import en from '~translations/en.json';
+import { ShowAlert } from '~utils/alert';
 
 export const onFulfilled = async (config: AxiosRequestConfig): Promise<any> => {
   // TODO: get user
@@ -29,6 +30,11 @@ export const onFulfilled = async (config: AxiosRequestConfig): Promise<any> => {
 export const netCheck = async (config: AxiosRequestConfig): Promise<any> => {
   const state = await NetInfo.fetch();
   if (!state?.isConnected) {
+    ShowAlert({
+      message: en.ERRORS.SOMETHING_WENT_WRONG,
+      description: en.ERRORS.NO_INTERNET,
+      type: 'danger',
+    });
     throw new axios.Cancel(en.ERRORS.NO_INTERNET);
   }
   return config;
